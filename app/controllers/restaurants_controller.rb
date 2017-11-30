ebd70a03b437c2d5f36f300b682ca8246cdfccc9
Morrie <morriedog@hotmail.com>
2017-11-30 09:43:59 +0800

    add user in out

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate!, except: [:index, :show]
+
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def edit
+    
+  end
+
+  def delete
+    
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  namespace :admit do 
+  namespace :admit do
     root "restaurants#index"
   end
   
