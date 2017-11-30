4727ccd1df40825bd3f143704d4482c6545b8b00
Morrie <morriedog@hotmail.com>
2017-11-30 10:24:23 +0800

    add new restaurant view and controller

--- app/controllers/admit/restaurants_controller.rb
+++ app/controllers/admit/restaurants_controller.rb
@@ -2,4 +2,29 @@ class Admit::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admit
 
+  def index
+    
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
 end
