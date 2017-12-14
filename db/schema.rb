8e1421eff1732562cf7b75aaf58c90690c61ef24
Morrie <morriedog@hotmail.com>
2017-12-14 09:06:58 +0800

    add category model and foreign_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130024253) do
+ActiveRecord::Schema.define(version: 20171214010524) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,6 +27,8 @@ ActiveRecord::Schema.define(version: 20171130024253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
