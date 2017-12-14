b399e6ec6c3d182c6f9a058f8f5cf0814a1dd039
Morrie <morriedog@hotmail.com>
2017-12-14 11:40:48 +0800

    add base_controller to admin and move all admin set to the file

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,4 @@
-class Admin::RestaurantsController < ApplicationController
-  before_action :authenticate_admin
+class Admin::RestaurantsController < Admin::BaseController
   before_action :find_restaurant, only: [:show, :edit, :destroy, :update]
 
   def index
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,5 @@ class ApplicationController < ActionController::Base
 
   private
   
-  def authenticate_admin
-    unless !current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
 
 end
