3782e04ccd39b47f6179b398155a1a3262d9374d
Morrie <morriedog@hotmail.com>
2017-12-28 15:38:24 +0800

    add comment table&model and relative with use and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,9 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader 
   belongs_to :category
 
+  has_many :comments, dependent: :destroy
+
+
   def self.search(search)
     where("name LIKE ?", "%#{search}%")
   end
