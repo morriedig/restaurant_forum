b5ce8a82df9b5188d13b6da83bce17eeee8753e0
Morrie <morriedog@hotmail.com>
2017-12-28 16:06:03 +0800

    can add comment in show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.restaurant_id = params[:restaurant_id]
+    @comment.save
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :restautant_id)
+  end
+
+
 end
