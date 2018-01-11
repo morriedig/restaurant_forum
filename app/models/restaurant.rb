757da6e08100aefaf29e6e99a5646370b29ef7c7
Morrie <morriedog@hotmail.com>
2018-01-11 15:54:07 +0800

    delete belongs to user in restaurant's model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader 
   belongs_to :category
-  belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
